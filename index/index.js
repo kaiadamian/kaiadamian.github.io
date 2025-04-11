d710be8d999acfc7b638b7ab70614f7bce3ef997
kaiadamian <kaiagabsdamian@gmail.com>
2025-04-11 18:47:17 -0400

    website

---
+++ index/index.js
@@ -0,0 +1,15 @@
+email = "kdamian@nd.edu";
+url = "https://github.com/kaiadamian?tab=repositories"
+pdf = "../documents/Damian_Kaia_Resume.pdf"
+
+function showEmail () {
+    alert(email);
+}
+
+function openUrl() {
+    window.location.href = url;
+}
+
+function openPdf() {
+    window.open(pdf, "_blank")
+}
